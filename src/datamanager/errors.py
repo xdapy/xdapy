cd38f9ed3c8dfeb863a606fe475233664baf5d0e
Hannah Dold <hannah.dold@mailbox.tu-berlin.de>
2009-06-23 17:15:46 +0200

    Provide exceptions for this module.

---
+++ src/datamanager/errors.py
@@ -0,0 +1,20 @@
+"""Provides the exceptions used in this module.
+
+Created on Jun 23, 2009
+
+    Error:         Base class for exceptions in this module.
+    AmbiguousObjectError: A list is returned instead of single object.
+   
+"""
+# alphabetical order by last name, please
+__authors__ = ['"Hannah Dold" <hannah.dold@mailbox.tu-berlin.de>']
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class AmbiguousObjectError(Error):
+    """Raised when an operation attempts to load a list of objects when only a 
+    single object is allowed.
+    """
+    pass
