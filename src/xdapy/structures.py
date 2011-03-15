da165b41d3815bf7c1891268c01816ade99d7ef9
Rike-Benjamin Schuppner <rikebs@debilski.de>
2011-03-15 18:10:08 +0100

    Adding some Entity creation tests.

--- src/xdapy/structures.py
+++ src/xdapy/structures.py
@@ -196,24 +196,30 @@ class Entity(Base):
 
 class Meta(DeclarativeMeta):
     @staticmethod
-    def _calculate_polymorphic_name(name, bases, attrs):
-        if not "EntityObject" in [cls.__name__ for cls in bases]:
-            return name
-
-        if "_" in cls.__name__:
-            raise EntityDefinitionError("Entity class must not contain an underscore")
-
-        # create hash from sorted parameter_types
-        parameter_types = attrs["parameter_types"]
+    def _hash_dict(attrs):
+        """Generates a hash from a simple dict containing string keys and values."""
         string = "{"
-        ordered = ['"' + k.lower() + '":"' + parameter_types[k].lower() + '"'
-                         for k in sorted(parameter_types.keys())]
+        ordered = ['"' + k.lower() + '":"' + attrs[k].lower() + '"'
+                         for k in sorted(attrs.keys())]
         string = "{" + ",".join(ordered) + "}"
 
         import hashlib
         md5 = hashlib.md5()
         md5.update(string)
         the_hash = md5.hexdigest()
+        return the_hash
+
+    @staticmethod
+    def _calculate_polymorphic_name(name, bases, attrs):
+        if not "EntityObject" in [bscls.__name__ for bscls in bases]:
+            return name
+
+        if "_" in name:
+            raise EntityDefinitionError("Entity class must not contain an underscore")
+
+        # create hash from sorted parameter_types
+        parameter_types = attrs["parameter_types"]
+        the_hash = Meta._hash_dict(parameter_types)
 
         return name + "_" + the_hash
 
---
+++ src/xdapy/test/test_entity.py
@@ -0,0 +1,53 @@
+"""Entity basic tests."""
+
+# alphabetical order by last name, please
+__authors__ = ['"Rike-Benjamin Schuppner <rikebs@debilski.de>"']
+
+from xdapy.structures import EntityObject, Entity, Data, Context
+from xdapy.errors import EntityDefinitionError
+import unittest
+
+
+parameter_types = {
+                'project': 'string',
+                'experimenter': 'string'
+            }
+
+parameter_types_w_date = {
+                'project': 'string',
+                'experimenter': 'string',
+                'project_start': 'date'
+            }
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        class Experiment(EntityObject):
+            parameter_types = parameter_types
+
+        class ExperimentalProject(EntityObject):
+            parameter_types = parameter_types
+        
+        self.Experiment = Experiment
+        self.ExperimentalProject = ExperimentalProject 
+    
+    def test_same_entity_has_same_hash(self):
+        class Experiment(EntityObject):
+            parameter_types = parameter_types
+        assert self.Experiment.__name__ == Experiment.__name__
+
+    def test_different_entity_has_different_hash(self):
+        class Experiment(EntityObject):
+            parameter_types = parameter_types_w_date
+        assert self.Experiment.__name__ != Experiment.__name__
+
+        assert self.Experiment.__name__ != self.ExperimentalProject.__name__
+
+    def test_entities_must_not_contain_underscore(self):
+        def mkEntity():
+            return type("Some_Entity", (EntityObject, ), { "parameter_types": {} })
+        self.assertRaises(EntityDefinitionError, mkEntity)
+        
+
+if __name__ == "__main__":
+    unittest.main()
