64633ab7b12f3d00c92ccaeec36c9039e6034e47
Rike-Benjamin Schuppner <rikebs@debilski.de>
2011-05-13 00:26:15 +0200

    WIP: Adding a stack trace to traversal.

--- src/operator.py
+++ src/operator.py
@@ -1,49 +1,52 @@
 
 class _BooleanOperator(object):
-    def __init__(self, inner):
+    def __init__(self, inner, stack=None):
         self.inner = inner
+        self.stack = stack or []
 
-        def traverse(inner):
+        def traverse(inner, stack):
             if isinstance(inner, tuple):
                 if len(inner) != 2:
                     raise "Too long or too short"
 
                 key = inner[0]
                 value = inner[1]
-                res = traverse(inner[1])
+#                res = traverse(inner[1])
+
+                stack = stack + [key]
 
                 if key == "_any":
-                    return _any(res)
+                    return _any(value, stack)
                 if key == "_all":
-                    return _all(res)
+                    return _all(value, stack)
 
                 if key == "_parent":
-                    return _parent(res)
+                    return _parent(value, stack)
 
-                return (key, res)
+                return (key, traverse(value, stack))
             if isinstance(inner, dict):
-                return traverse(("_all", inner.items()))
+                return traverse(("_all", inner.items()), stack)
             if isinstance(inner, list):
-                return [traverse(i) for i in inner]
+                return [traverse(i, stack) for i in inner]
 
             return inner
         
-        self.inner = traverse(self.inner)
+        self.inner = traverse(self.inner, self.stack)
 
     def __repr__(self):
         return repr(self.inner)
 
 class _any(_BooleanOperator):
     def __repr__(self):
-        return " _any( " + repr( self.inner ) + " ) "
+        return " _any( " + repr( self.inner ) + "{"+ str(self.stack) + "} ) "
 
 class _all(_BooleanOperator):
     def __repr__(self):
-        return " _all( " + repr( self.inner ) + " ) "
+        return " _all( " + repr( self.inner ) + "{"+ str(self.stack) + "} ) "
 
 class _parent(_BooleanOperator):
     def __repr__(self):
-        return " _parent( " + repr( self.inner ) + " ) "
+        return " _parent( " + repr( self.inner ) + "{"+ str(self.stack) + "} ) "
 
 
 
