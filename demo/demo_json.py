33b15e5554cda9d928253889212f5ec19fbcd4ba
Rike-Benjamin Schuppner <rikebs@debilski.de>
2012-02-16 14:36:44 +0100

    DOC: Include JSON demo.

---
+++ demo/demo_json.py
@@ -0,0 +1,23 @@
+
+from xdapy import Connection, Mapper
+from xdapy.io import JsonIO
+
+connection = Connection.profile("test") # use standard profile
+connection.create_tables(overwrite=True)
+m = Mapper(connection)
+
+# We create a new JSON importer.
+# add_new_types = True so all defined and declared objects are imported
+jio = JsonIO(m, add_new_types=True)
+jio.ignore_unknown_attributes = True
+
+with open("demo/detection.json") as f:
+    objs = jio.read_file(f)
+
+for cls in m.registered_objects:
+    print """class {name}(EntityObject):\n    parameter_types = {types!r}\n""".format(name=cls.__original_class_name__, types=cls.parameter_types)
+
+print len(objs), "objects"
+
+print jio.write_string(objs)
+
