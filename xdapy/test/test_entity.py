2546db6e84cea8745a6c272c5f015e76f1b9abad
Rike-Benjamin Schuppner <rikebs@debilski.de>
2012-06-07 17:21:46 +0200

    UT: Adding tests for unique_id.

--- xdapy/test/test_entity.py
+++ xdapy/test/test_entity.py
@@ -1,6 +1,8 @@
 """Entity basic tests."""
 
 # alphabetical order by last name, please
+from sqlalchemy.exc import IntegrityError
+
 __authors__ = ['"Rike-Benjamin Schuppner" <rikebs@debilski.de>']
 
 from xdapy import Connection, Mapper, Entity
@@ -88,6 +90,67 @@ class TestSavedTypes(unittest.TestCase):
     def test_polymorphic_id_is_type(self):
         ent = self.MyTestEntity(some_param = "a short string")
         self.assertEqual(ent._type, ent.__mapper_args__["polymorphic_identity"])
+
+
+class NameEntityAutoId(Entity):
+    declared_params = {
+        "first_name": "string",
+        "last_name": "string",
+        }
+
+class NameEntity(Entity):
+    declared_params = {
+        "first_name": "string",
+        "last_name": "string",
+        }
+    def gen_unique_id(self):
+        return "%s %s" % (self.params["first_name"], self.params["last_name"])
+
+
+class TestUniqueId(unittest.TestCase):
+    def setUp(self):
+        self.connection = Connection.test()
+        self.connection.create_tables()
+        self.m = Mapper(self.connection)
+        self.m.register(NameEntityAutoId)
+        self.m.register(NameEntity)
+
+    def test_unique_id(self):
+        user = NameEntity(first_name="David", last_name="Persaud")
+        self.assertIsNone(user._unique_id)
+        self.m.save(user)
+        self.assertEqual(user.unique_id, "David Persaud")
+
+        # does not work twice
+
+        user2 = NameEntity(first_name="David", last_name="Persaud")
+        self.assertIsNone(user2._unique_id)
+        self.assertRaises(IntegrityError, self.m.save, user2)
+        # setting it manually
+        user2._unique_id = "David Persaud (II)"
+        # now it works
+        self.m.save(user2)
+        self.assertEqual(user2.unique_id, "David Persaud (II)")
+
+    def test_auto_gen_unique_id(self):
+        user = NameEntityAutoId(first_name="David", last_name="Persaud")
+        self.assertIsNone(user._unique_id)
+        self.m.save(user)
+        # it is something completely different
+        self.assertIsNotNone(user._unique_id)
+        self.assertNotEqual(user.unique_id, "David Persaud")
+
+        # does work twice
+        
+        user2 = NameEntityAutoId(first_name="David", last_name="Persaud")
+        self.assertIsNone(user2._unique_id)
+        self.m.save(user2)
+        # it is something completely different
+        self.assertIsNotNone(user2._unique_id)
+        self.assertNotEqual(user2.unique_id, "David Persaud")
+
+        self.assertNotEqual(user._unique_id, user2._unique_id)
+
 
 
 if __name__ == "__main__":
